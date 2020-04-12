5e3c9d17002228f2e915062bc10c5f9d7ebad52b
ian proletov <yan.proletov@gmail.com>
2020-04-12 23:57:39 +0300

    fix Ph value to double

--- src/ru/proletov/ckdmbd/models/Ph.java
+++ src/ru/proletov/ckdmbd/models/Ph.java
@@ -2,7 +2,7 @@ package ru.proletov.ckdmbd.models;
 
 public class Ph extends LaboratoryInvestigation {
 
-    public Ph(final float value) {
+    public Ph(final double value) {
         super(value);
     }
 
