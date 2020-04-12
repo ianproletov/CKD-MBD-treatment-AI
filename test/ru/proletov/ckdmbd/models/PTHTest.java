4ecb7e1dbebd2b7e612b60d39cf96634aa9875a6
ian proletov <yan.proletov@gmail.com>
2020-04-12 23:44:41 +0300

    add testSetValue

--- test/ru/proletov/ckdmbd/models/PTHTest.java
+++ test/ru/proletov/ckdmbd/models/PTHTest.java
@@ -16,6 +16,16 @@ public class PTHTest {
     }
 
     @Test
+    public void testSetValue() {
+        PTH pthResult = new PTH(150);
+        double inputValue = 400;
+        pthResult.setValue(inputValue);
+
+        assertEquals(inputValue, pthResult.getValue(), 0);
+
+    }
+
+    @Test
     public void testGetUnitOfMeasurement() {
         double inputValue = 150;
         PTH pthResult = new PTH(inputValue);
@@ -24,13 +34,24 @@ public class PTHTest {
     }
 
     @Test
-    public void testChangeUnitOfMeasurement() {
+    public void testChangeUnitOfMeasurement1() {
         double inputValue = 150;
         PTH pthResult = new PTH(inputValue);
         pthResult.changeUnitOfMeasurement(PTH.UnitOfMeasurement.pkmoll);
         double expectedValue = inputValue / PTH.GetConverter();
 
         assertEquals(PTH.UnitOfMeasurement.pkmoll, pthResult.getUnitOfMeasurement());
+        assertEquals(expectedValue, pthResult.getValue(), 0);
+    }
+
+    @Test
+    public void testChangeUnitOfMeasurement2() {
+        double inputValue = 9;
+        PTH pthResult = new PTH(inputValue, PTH.UnitOfMeasurement.pkmoll);
+        pthResult.changeUnitOfMeasurement(PTH.UnitOfMeasurement.pgml);
+        double expectedValue = inputValue * PTH.GetConverter();
+
+        assertEquals(PTH.UnitOfMeasurement.pgml, pthResult.getUnitOfMeasurement());
         assertEquals(expectedValue, pthResult.getValue(), 0);
     }
 
