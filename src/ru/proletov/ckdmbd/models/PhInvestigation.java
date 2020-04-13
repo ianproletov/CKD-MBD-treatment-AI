e50a4e4acbd204235a03c31136af0284f7a3508a
ian proletov <yan.proletov@gmail.com>
2020-04-14 00:39:59 +0300

    add changeUnitsOfMeasure feature

--- src/ru/proletov/ckdmbd/models/PhInvestigation.java
+++ src/ru/proletov/ckdmbd/models/PhInvestigation.java
@@ -2,6 +2,8 @@ package ru.proletov.ckdmbd.models;
 
 public class PhInvestigation extends AbstractInvestigation {
 
+    private static final double CONVERTER = 3.1;
+
     public PhInvestigation(final double value) {
         this(value, UnitOfMeasurement.mmoll);
     }
@@ -12,7 +14,25 @@ public class PhInvestigation extends AbstractInvestigation {
     }
 
     @Override
-    public PTHInvestigation changeUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) {
-        return null;
+    public PhInvestigation changeUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) {
+        if (unitOfMeasurement == this.unitOfMeasurement) return this;
+        double currentConverter = CONVERTER;
+        switch (unitOfMeasurement) {
+            case mgdl:
+                this.unitOfMeasurement = UnitOfMeasurement.mgdl;
+                break;
+            case mmoll:
+                this.unitOfMeasurement = UnitOfMeasurement.mmoll;
+                currentConverter = 1 / CONVERTER;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + unitOfMeasurement);
+        }
+        this.value = this.value * currentConverter;
+        return this;
+    }
+
+    public static double GetConverter() {
+        return CONVERTER;
     }
 }
