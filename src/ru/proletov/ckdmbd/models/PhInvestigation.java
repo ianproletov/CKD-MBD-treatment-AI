ce3ead3113e02296df08a5caf621b8f885315610
ian proletov <yan.proletov@gmail.com>
2020-04-14 01:34:26 +0300

    add valid units

--- src/ru/proletov/ckdmbd/models/PhInvestigation.java
+++ src/ru/proletov/ckdmbd/models/PhInvestigation.java
@@ -1,20 +1,30 @@
 package ru.proletov.ckdmbd.models;
 
+import ru.proletov.ckdmbd.models.exceptions.InvalidUnitOfMeasureException;
+
+import java.util.Arrays;
+
 public class PhInvestigation extends AbstractInvestigation {
 
     private static final double CONVERTER = 3.1;
 
-    public PhInvestigation(final double value) {
+    private static UnitOfMeasurement[] validUnits = {UnitOfMeasurement.mmoll, UnitOfMeasurement.mgdl};
+
+    public PhInvestigation(final double value) throws InvalidUnitOfMeasureException {
         this(value, UnitOfMeasurement.mmoll);
     }
 
-    public PhInvestigation(final double value, final UnitOfMeasurement unitOfMeasurement) {
+    public PhInvestigation(final double value, final UnitOfMeasurement unitOfMeasurement) throws InvalidUnitOfMeasureException {
         super(value);
-        this.unitOfMeasurement = unitOfMeasurement;
-    }
+        if (Arrays.asList(validUnits).contains(unitOfMeasurement)) {
+            this.unitOfMeasurement = unitOfMeasurement;
+        } else {
+            throw new InvalidUnitOfMeasureException(unitOfMeasurement);
+        }
+     }
 
     @Override
-    public PhInvestigation changeUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) {
+    public PhInvestigation changeUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) throws InvalidUnitOfMeasureException {
         if (unitOfMeasurement == this.unitOfMeasurement) return this;
         double currentConverter = CONVERTER;
         switch (unitOfMeasurement) {
@@ -26,7 +36,7 @@ public class PhInvestigation extends AbstractInvestigation {
                 currentConverter = 1 / CONVERTER;
                 break;
             default:
-                throw new IllegalStateException("Unexpected value: " + unitOfMeasurement);
+                throw new InvalidUnitOfMeasureException(unitOfMeasurement);
         }
         this.value = this.value * currentConverter;
         return this;
