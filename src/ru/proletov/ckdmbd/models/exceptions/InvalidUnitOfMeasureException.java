3600c39237e13963198b13f495aca0f20c615373
ian proletov <yan.proletov@gmail.com>
2020-04-14 01:56:27 +0300

    fix message part

--- src/ru/proletov/ckdmbd/models/exceptions/InvalidUnitOfMeasureException.java
+++ src/ru/proletov/ckdmbd/models/exceptions/InvalidUnitOfMeasureException.java
@@ -1,7 +1,17 @@
 package ru.proletov.ckdmbd.models.exceptions;
 
+import ru.proletov.ckdmbd.models.UnitOfMeasurement;
+
 public class InvalidUnitOfMeasureException extends Exception {
+
+    private UnitOfMeasurement unitOfMeasurement;
+    private final String message = "Can not to proceed this unit of measure";
+
+    public InvalidUnitOfMeasureException(final UnitOfMeasurement unitOfMeasurement) {
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+
     public String getMessage() {
-        return "Can not to proceed this unit of measure";
+        return (message + unitOfMeasurement);
     }
 }
