048bfe254b0c3a59cd2e6b5d7804781870bb09d2
ian proletov <yan.proletov@gmail.com>
2020-04-16 22:45:02 +0300

    fix format

--- test/ru/proletov/ckdmbd/controllers/RecommendationMakerTest.java
+++ test/ru/proletov/ckdmbd/controllers/RecommendationMakerTest.java
@@ -3,6 +3,9 @@ package ru.proletov.ckdmbd.controllers;
 import org.junit.Test;
 import ru.proletov.ckdmbd.models.*;
 import ru.proletov.ckdmbd.models.exceptions.InvalidUnitOfMeasureException;
+import ru.proletov.ckdmbd.models.investigations.CaInvestigation;
+import ru.proletov.ckdmbd.models.investigations.PTHInvestigation;
+import ru.proletov.ckdmbd.models.investigations.PhInvestigation;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +14,9 @@ public class RecommendationMakerTest {
     @Test
     public void makeRecommendation() throws InvalidUnitOfMeasureException {
 
-        PatientState patientState = new PatientState(new CaInvestigation(2.0), new PhInvestigation(1.3), new PTHInvestigation(800));
+        PatientState patientState = new PatientState(new CaInvestigation(2.0),
+                new PhInvestigation(1.3),
+                new PTHInvestigation(800));
         RecommendationMaker recommendationMaker = new RecommendationMaker();
         Recommendation recommendation = recommendationMaker.makeRecommendation(patientState);
 
