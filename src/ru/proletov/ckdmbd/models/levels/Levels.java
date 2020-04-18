29ed6c40b2123d91f86e1c031a7f90a6323d5891
ian proletov <yan.proletov@gmail.com>
2020-04-18 22:43:29 +0300

    refactor constant levels

---
+++ src/ru/proletov/ckdmbd/models/levels/Levels.java
@@ -0,0 +1,12 @@
+package ru.proletov.ckdmbd.models.levels;
+
+public final class Levels {
+    public static double CA_HI = 2.5;
+    public static double CA_LOW = 2.1;
+    public static double PH_HI = 1.78;
+    public static double PH_LOW = 0.87;
+    public static double PTH_HI = 600;
+    public static double PTH_LOW = 150;
+
+    private Levels() {}
+}
